04b6b6205f11358972a57660890f46006353dc3d
ngeller0720 <nicholasgeller0720@gmail.com>
2025-04-18 11:35:26 -0400

    Update Fruit Insights Generator.py

--- pages/Fruit Insights Generator.py
+++ pages/Fruit Insights Generator.py
@@ -3,7 +3,7 @@ import requests
 import google.generativeai as genai
 import os
 
-genai.configure(api_key=st.secrets["gemini"]["AIzaSyCqspAIoxulvpnWNcSxmBlv77QP-PUgwiE"])
+genai.configure(api_key=st.secrets["gemini"]["api_key"])
 
 model = genai.GenerativeModel("gemini-1.5-flash")
 
